7dce25075041fa32955398b8e5636fb5299b9038
SHEF <Shefqetmush@gmail.com>
2022-08-26 19:51:21 -0400

    function operate()

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ let currentValue = "";
 let previousValue = "";
 let operation = "";
 let previousOperation = "";
+
+function operate(currentValue, previousValue, operation) {
+  if (operation == "*") {
+    return currentValue * previousValue;
+  } else if (operation == "+") {
+    return currentValue + previousValue;
+  } else if (operation == "-") {
+    return currentValue - previousValue;
+  } else if (operation == "/") {
+    return currentValue / previousValue;
+  }
+}
+
+console.log(operate(50, 50, "/"));

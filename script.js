f6de6907a5b8594e485b425e2f6155a0d2c9bca4
SHEF <Shefqetmush@gmail.com>
2022-08-26 20:24:35 -0400

    functionality work

--- script.js
+++ script.js
@@ -2,7 +2,34 @@ let currentValue = "";
 let previousValue = "";
 let operation = "";
 let previousOperation = "";
+let input = document.querySelector("input");
 
+// on button press run buttonPressed
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", buttonPressed));
+
+//on one of the operations being pressed run operationPress
+let operationSigns = document.querySelectorAll(".operation");
+operationSigns.forEach((button) =>
+  button.addEventListener("click", () => operationPress(button.value))
+);
+
+//set calculator input screen to the buttons pressed
+function buttonPressed() {
+  currentValue += this.value;
+  input.value += this.value;
+  return currentValue;
+}
+
+//on operation press
+function operationPress(value) {
+  let operation = value;
+  previousValue = currentValue;
+  currentValue = "";
+  return previousValue, operation, currentValue;
+}
+
+//operate operation on paramaters
 function operate(currentValue, previousValue, operation) {
   if (operation == "*") {
     return currentValue * previousValue;
@@ -14,5 +41,3 @@ function operate(currentValue, previousValue, operation) {
     return currentValue / previousValue;
   }
 }
-
-console.log(operate(50, 50, "/"));
